28ad295acbb769c636d8a5d031bfb71265206b40
Jas Strong <jasmine@electronpusher.org>
2013-10-16 11:13:16 -0700

    Make the bridge remember which pushers it's seen
    The remap process can take a second or two.  We should remember which
    pushers we've seen, and not remap them unless we really need to.

--- src/com/heroicrobot/pixelpusher/artnet/PixelPusherObserver.java
+++ src/com/heroicrobot/pixelpusher/artnet/PixelPusherObserver.java
@@ -1,20 +1,51 @@
 package com.heroicrobot.pixelpusher.artnet;
 
+import java.util.HashMap;
 import java.util.Observable;
+
 import java.util.Observer;
 
+import com.heroicrobot.dropbit.devices.pixelpusher.PixelPusher;
 import com.heroicrobot.dropbit.registry.DeviceRegistry;
 
 class PixelPusherObserver implements Observer {
   public boolean hasStrips = false;
   public ArtNetMapping mapping = new ArtNetMapping();
-
+  public HashMap<String,PixelPusher> knownPushers = new HashMap<String,PixelPusher>();
+  
+  public boolean hasSignificantChange(PixelPusher updatedDevice) {
+	  PixelPusher known = knownPushers.get(updatedDevice.getMacAddress());
+	  if (known == null) {
+		  knownPushers.put(updatedDevice.getMacAddress(), updatedDevice);
+		  return true;
+	  }
+	  if (known.getNumberOfStrips() != updatedDevice.getNumberOfStrips()) {
+		  knownPushers.put(updatedDevice.getMacAddress(), updatedDevice);
+		  return true;  
+	  }
+	  if (known.getPixelsPerStrip() != updatedDevice.getPixelsPerStrip()) {
+		  knownPushers.put(updatedDevice.getMacAddress(), updatedDevice);
+		  return true;  
+	  }
+	  
+	  // otherwise it's not a significant enough change to trigger a remap, but we should remember that it changed.
+	  knownPushers.put(updatedDevice.getMacAddress(), updatedDevice);
+	  return false;
+  }
+  
   public void update(Observable registry, Object updatedDevice) {
      //logging.info("Registry changed!");
     if (updatedDevice != null) {
-      generateMapping((DeviceRegistry) registry);
-       System.out.println("Device change: " + updatedDevice);
-    }
+    	if (updatedDevice instanceof PixelPusher) {
+    		if (hasSignificantChange((PixelPusher)updatedDevice)) {
+    			generateMapping((DeviceRegistry) registry);
+    			
+        		System.out.println("Device change: " + updatedDevice);
+    		}
+    	} else {
+    		System.out.println("Registry:  updated device was not a PixelPusher!");
+    	}
+  	}
     this.hasStrips = true;
   }
 
