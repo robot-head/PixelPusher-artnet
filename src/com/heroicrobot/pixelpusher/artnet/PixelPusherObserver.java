15ed79e0dc19aee639f71dc6e099c20db7675081
Jas Strong <jasmine@electronpusher.org>
2013-10-23 01:32:30 -0700

    Make sACN kinda work
    Fix the check for valid data.  Correct the offset calculations (which,
    it turns out, were wrong).

--- src/com/heroicrobot/pixelpusher/artnet/PixelPusherObserver.java
+++ src/com/heroicrobot/pixelpusher/artnet/PixelPusherObserver.java
@@ -1,7 +1,6 @@
 package com.heroicrobot.pixelpusher.artnet;
 
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Observable;
 
--- src/com/heroicrobot/pixelpusher/artnet/SacnReceiver.java
+++ src/com/heroicrobot/pixelpusher/artnet/SacnReceiver.java
@@ -1,15 +1,10 @@
 package com.heroicrobot.pixelpusher.artnet;
 
-// 239.255.0.1
-
 import java.io.IOException;
 import java.net.DatagramPacket;
-import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.InetSocketAddress;
 import java.net.MulticastSocket;
-import java.net.NetworkInterface;
-import java.util.Enumeration;
+
 
 public class SacnReceiver extends Thread {
 
@@ -35,26 +30,26 @@ public class SacnReceiver extends Thread {
 		  uint16_t preamble;				// 0
 		  uint16_t postamble;				// 2
 		  uint8_t  packetId		[12];		// 4
-		  uint16_t flagsLength;				// 14
-		  uint8_t  vector		[4];		// 16
-		  uint8_t  CID			[16];		// 20
+		  uint16_t flagsLength;				// 16
+		  uint8_t  vector		[4];		// 18
+		  uint8_t  CID			[16];		// 22
 		  //Framing Layer
-		  uint16_t frmFlagsLength;			// 36
-		  uint8_t  frmVector	[4];		// 38
-		  uint8_t  sourceName	[64];		// 42
-		  uint8_t  priority;				// 106
-		  uint8_t  reservedIgnore	[2];	// 107
-		  uint8_t  seqNo;					// 109
-		  uint8_t  options;					// 110
-		  uint8_t  universeNo      [2];		// 111
+		  uint16_t frmFlagsLength;			// 38
+		  uint8_t  frmVector	[4];		// 40
+		  uint8_t  sourceName	[64];		// 44
+		  uint8_t  priority;				// 108
+		  uint8_t  reservedIgnore	[2];	// 109
+		  uint8_t  seqNo;					// 111
+		  uint8_t  options;					// 112
+		  uint8_t  universeNo      [2];		// 113
 		  //DMP Layer
-		  uint16_t dmpFlagsLength;			// 113
-		  uint8_t  dmpVector;				// 115
-		  uint8_t  addrType;				// 116
-		  uint16_t firstPropertyAddr;		// 118
-		  uint16_t addrIncrement;			// 120
-		  uint16_t propValCount;			// 122
-		  uint8_t  dataValues	[513];		// 124  NOTE:  dmx data starts at dataValues + 1
+		  uint16_t dmpFlagsLength;			// 115
+		  uint8_t  dmpVector;				// 117
+		  uint8_t  addrType;				// 118
+		  uint16_t firstPropertyAddr;		// 120
+		  uint16_t addrIncrement;			// 122
+		  uint16_t propValCount;			// 124
+		  uint8_t  dataValues	[513];		// 126  NOTE:  dmx data starts at dataValues + 1
 		  
 		} __attribute__((packed));
 	 */
@@ -65,8 +60,6 @@ public class SacnReceiver extends Thread {
 		    this.seenPacket = false;
 		    try {
 				mcSocket = new MulticastSocket(SACN_PORT);
-			//	mcSocket.setReuseAddress(true);
-				//mcSocket.bind(new InetSocketAddress(InetAddress.getByName("0.0.0.0"), SACN_PORT));
 			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
@@ -116,45 +109,33 @@ public class SacnReceiver extends Thread {
 	
 	  private void parseSacnPacket(DatagramPacket packet) {
 		    buf = packet.getData();
+		    if (buf.length > ACN_IDENTIFIER_SIZE) {
 		    for (int i = 0; i < ACN_IDENTIFIER_SIZE; i++) {
-		      if (ACN_IDENTIFIER[i] != buf[i + 4]) { // packetId
-		    	  System.out.println("Got a packet on the sACN port, but ID was wrong.");
-		    	  return;
-		      }
+		    		if (ACN_IDENTIFIER[i] != buf[i + 4]) { // packetId
+		    	  		System.out.println("Got a packet on the sACN port, but ID was wrong.");
+		    	  		return;
+		      		}
+		    	}
 		    }
 		    // If we get here, it looks like there's a packet to handle.
-		    if (buf[8] == 0x00 && buf[9] == 0x50) {
-		      // Opcode 0x5000 is DMX data
+		    
 		      if (!this.seenPacket) {
 		        System.out.println("Got an sACN packet!");
 		        this.seenPacket = true;
 		      }
-		      int universe = (buf[111] | (buf[112] << 8)) + 1;
+		      int universe = (buf[114] | (buf[113] << 8));
+		      //System.out.println("Universe = "+universe);
 		      for (int i = 0; i < 512; i++) {
 		        // the channel data is in buf[i+124];
-		        update_channel(universe, i + 1, buf[i + 125]);
+		        update_channel(universe, i + 1, buf[i + 127]);
 		      }
-		    } 
+		     
 		  }
 	
 	  @Override
 	  public void run() {
-	  //  DatagramSocket socket = null;
 	    DatagramPacket packet = new DatagramPacket(buf, buf.length);
 
-	  //  try {
-	      // socket = new DatagramSocket(null);
-	      
-	     // socket.setReuseAddress(true);
-	     // socket.setBroadcast(true);
-	      
-	     // socket.bind(new InetSocketAddress(InetAddress.getByName("0.0.0.0"), SACN_PORT));
-	     // System.out.println("Listening for streaming ACN messages on " + socket.getLocalAddress() + " port "
-	     //     + socket.getLocalPort() + ", broadcast=" + socket.getBroadcast());
-	  //  } catch (IOException e) {
-	   //   e.printStackTrace();
-	   //   return;
-	   // }
 	    int packetno = 0;
 	    while (true) {
 	      try {
