b034b9ecf2fa56bdaac4b2f778306a2dcdcd41cc
Jas Strong <jasmine@electronpusher.org>
2014-11-15 12:30:10 -0800

    Add warning suppression for daemon socket
    We never close it because why would we close it?

--- src/com/heroicrobot/pixelpusher/artnet/ArtNetReceiver.java
+++ src/com/heroicrobot/pixelpusher/artnet/ArtNetReceiver.java
@@ -372,6 +372,7 @@ public class ArtNetReceiver extends Thread {
 
   
 // Suppress the warning about never closing our daemon socket.
+@SuppressWarnings("resource")
 @Override
   public void run() {
     DatagramSocket socket = null;
